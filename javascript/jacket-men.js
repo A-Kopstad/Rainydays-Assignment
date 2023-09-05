f9e88544c68c9245258dfe71a1938d32316c5afd
A-Kopstad <126067888+A-Kopstad@users.noreply.github.com>
2023-09-05 14:47:22 +0200

    changes 5th september

---
+++ javascript/jacket-men.js
@@ -0,0 +1,38 @@
+
+const productdetailsContainer = document.querySelector(".product-container-outer");
+
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get("id");
+
+
+
+const productDetailsUrl = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
+productdetailsContainer.innerHTML = `<div class="loading"></div>`;
+async function apiCall() {
+    try {
+        const response = await fetch(productDetailsUrl);
+        const result = await response.json();
+
+        await new Promise(resolve => setTimeout(resolve, 2000)); //ta bort
+
+        productdetailsContainer.innerHTML = "";
+
+        productdetailsContainer.innerHTML += `
+            <div class="product-container-inner">
+                <div class="row1">
+                    <div class="jacket-container">
+                        <a href="/html/productpage.html">
+                            <img class="jacket" src="${result.image}" alt="${result.title}" />
+                        </a>
+                    </div>
+                    <p class="text">${result.description}&nbsp;</p>
+                </div>
+            </div>`;
+
+    } catch (error) {
+        productdetailsContainer.innerHTML = "An error has occurred";
+        console.error('Error fetching product details:', error);
+    }
+}
+
+apiCall();
