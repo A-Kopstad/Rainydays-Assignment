264591c0a7a8d14e4fc65b96fdc4aa875699e8a2
A-Kopstad <126067888+A-Kopstad@users.noreply.github.com>
2023-10-29 19:14:40 +0100

    Update productCatalog.js

--- javascript/productCatalog.js
+++ javascript/productCatalog.js
@@ -19,6 +19,9 @@ async function apiCall() {
             }
         });
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const result = await response.json();
 
         productContainer.innerHTML = "";
